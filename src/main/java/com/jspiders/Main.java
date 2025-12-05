1c002305347c0e04c9b975201e8bd318d4b2b996
divyaboppana <boppanadivya001@gmail.com>
2025-12-05 11:45:27 +0530

    print statment

--- src/main/java/com/jspiders/Main.java
+++ src/main/java/com/jspiders/Main.java
@@ -5,6 +5,7 @@ import org.hibernate.Session;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("bye freinds");
 
             }
 }
