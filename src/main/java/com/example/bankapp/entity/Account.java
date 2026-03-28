385eac9b9b9931f816b3e4b364eb849c9929d228
poojithaofficial2003-ctrl <poojithaofficial2003@gmail.com>
2026-03-28 07:54:54 +0530

    Update Account.java

--- src/main/java/com/example/bankapp/entity/Account.java
+++ src/main/java/com/example/bankapp/entity/Account.java
@@ -11,7 +11,7 @@ public class Account {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true) // ✅ IMPORTANT
+    @Column(unique = true) 
     private String accountNumber;
 
     private double balance;
